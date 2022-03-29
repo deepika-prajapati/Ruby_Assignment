01910a1e7e7f479c9a6f7803db12fc8bdc411193
deepika-prajapati <102571447+deepika-prajapati@users.noreply.github.com>
2022-03-29 21:39:11 +0530

    Update Dictionary.rb

--- Dictionary.rb
+++ Dictionary.rb
@@ -1,38 +1,77 @@
-dictionary = {"key1" => "value1", "key2" => "value2"}
-while 1==1
- puts ""
-    puts "1. Search"
-    puts "2. Add"
-    puts "3. Update"
-    puts "4. Delete"
-    puts "5. View"
-    puts "Please choose any option"
-    option = gets.chomp.to_i
-    case option
-    when 1
-        puts "Enter Key to Search"
-        key1 = gets.chomp
-        #rs = hash.key(key1)
-        #puts rs
-    when 2
+class Dictionary
+    def add(dictionary)
         puts "Enter Key to add"
         key = gets.chomp
         puts "Enter Value"
         value = gets.chomp
         dictionary.store(key,value)
-    when 3
-         puts "Enter Key to update"
+        puts "Added Successfully !!!"
+    end
+    def update(dictionary)
+        puts "Enter Key to update"
         key = gets.chomp
-        puts "Enter new Value"
-        value = gets.chomp
-        dictionary.store(key,value)
-    when 4
+        
+        rs = dictionary[key].to_s
+        if rs.length ==0
+            puts "Key not found"
+        else
+            puts "Enter new Value"
+            value = gets.chomp
+            dictionary.store(key,value)
+            puts "Updated Successfully !!!"
+        end
+    end
+    def delete(dictionary)
         puts "Enter Key to delete"
-        key = gets.chomp
-     dictionary.delete(key)
+        key1 = gets.chomp
+        rs = dictionary[key1].to_s
+        if rs.length ==0
+            puts "Key not found"
+        else
+            dictionary.delete(key1)
+            puts "Deleted Successfully !!!"
+        end
+    end
+    def search(dictionary)
+        puts "Enter Key to Search"
+        key1 = gets.chomp
+        rs = dictionary[key1].to_s
+        if rs.length ==0
+            puts "Key not found"
+        else
+            print "value of ",key1, " : ", rs
+        end
+    end
+    def view(dictionary)
+        puts dictionary
+    end
+    def menu()
+        puts ""
+        puts "1. View"
+        puts "2. Add"
+        puts "3. Update"
+        puts "4. Delete"
+        puts "5. Search"
+        puts "Please choose any option"
+    end
+end
+dictionary = {"key1" => "value1", "key2" => "value2"}
+dc = Dictionary.new
+while 1==1
+    dc.menu()
+    option = gets.chomp.to_i
+    case option
+    when 1
+        dc.view(dictionary)
+    when 2
+        dc.add(dictionary)
+    when 3
+        dc.update(dictionary)
+    when 4
+        dc.delete(dictionary)
      when 5
-    puts dictionary
-    else
-     puts "Invalid option"
+       dc.search(dictionary)
+    else        
+        puts "Invalid option"
     end
 end
