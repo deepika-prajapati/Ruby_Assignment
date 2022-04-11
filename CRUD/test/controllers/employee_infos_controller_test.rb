ef85c2a2b9a84e47e31d35c5d3455100a47fd104
deepika-prajapati <102571447+deepika-prajapati@users.noreply.github.com>
2022-04-11 12:38:02 +0530

    CRUD

--- CRUD/test/controllers/employee_infos_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class EmployeeInfosControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get employee_infos_index_url
-    assert_response :success
-  end
-end
